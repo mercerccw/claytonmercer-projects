45b27799adeef86b8b30ed4452312df4aaaf502d
Clayton Mercer <mercerccw@gmail.com>
2020-08-21 10:40:28 -0400

    Route change

--- src/routes/project.js
+++ src/routes/project.js
@@ -4,10 +4,10 @@ const router = express.Router();
 // router.get('/getAllProjects', (req, res) => {
 //     res.send('Got them!');
 // });
-router.get('/getAllProjects', (req, res) => {
+router.get('/', (req, res) => {
     ProjectController.getAllProjects(req, res);
 });
-router.get('/getProject/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     ProjectController.getSingleProject(req, res);
 });
 module.exports = router;
